e0d71a13ff743589713e021b779a2da536d7cf67
JoseToroB <65199659+JoseToroB@users.noreply.github.com>
2021-08-05 02:19:48 -0400

    comenzado el control
    terminados los tdas, faltan los metodos de control

---
+++ SRC/CONTROL/Control.java
@@ -0,0 +1,18 @@
+package CONTROL;
+
+public class Control {
+    //atributos
+    //el control tiene como atributos las clases del modelo
+    //modelo
+    private RS redSocial;
+    //constructor
+    public Control(RS redSocial) {
+        this.redSocial = redSocial;
+    }
+    //getters 
+    public RS getRedSocial() {
+        return redSocial;
+    }
+    //metodos
+    //ahora creare los metodos solicitados en el laboratorio
+}
